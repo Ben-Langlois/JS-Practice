5ba179c787f5710787c92ede1b312bf3c96c141a
Ben_Langlois <benlangloiss@gmail.com>
2021-04-10 20:58:14 -0400

    added some high order arrray functions, added more array functions

--- JSArrayFunctions.js
+++ JSArrayFunctions.js
@@ -117,6 +117,10 @@ let arrOfObj = [{x: 0}, {x: 1}, {x: 2}, {x: 3}, {x: 4}, {x: 5}];
  - executes a callback/reader function to each element of an array
  - mutates og array
  - not sure the point yet besides getting sum of all numbers
+ - executes the callback function on each member of the calling array which 
+   results in a single output value. 
+ - The reduce method accepts two parameters: 1) The reducer function (callback), 2) 
+   and an optional initialValue.
 */
 
 // cycle through arr and add numbers together
@@ -152,13 +156,109 @@ function compareFunction(a, b) {
 }
 */
 // lets try without call back
-console.log(randomNums.sort());                 //[0, 123, 14, 2, 234, 25, 42, 5, 53, 63, 74]
+// console.log(randomNums.sort());                 //[0, 123, 14, 2, 234, 25, 42, 5, 53, 63, 74]
 // only really sorts first number of element 
 // although [1, 12, 14, 13].sort() if would give 1, 12, 13, 14
 
 // properly sorting by ascending
-console.log(randomNums.sort((a,b) => a - b));
+//console.log(randomNums.sort((a,b) => a - b));
 
 // #endregion
 
-// Problems
+// #region .some
+/* array.some(function)
+ - returns true/false depending on passed function
+ - used to see if some of the values pass the criteria
+*/
+
+// const even = (e) => e % 2 === 0;                // returns true if its an even number
+
+// console.log([1, 2, 3, 4, 5].some(even));        // true
+// console.log([2, 4, 6, 8, 10].some(even));       // true
+// console.log([1, 3, 5, 7, 9].some(even));        // false
+// #endregion
+
+// #region .every
+/* array.all(function)
+ - like some but only returns true if all elements of array meet criteria
+*/ 
+
+// using even callback from .some section
+// console.log([1, 2, 3, 4, 5].every(even));       // false
+// console.log([2, 4, 6, 8, 10].every(even));      // true
+// console.log([2, 4, 6, 8, 9].every(even));       // false (because of [9])
+// #endregion
+
+// #region  Problems (prefferably combining the four)
+// find all even numbers, halve them and return them in order
+
+// const evenNumHalfinator = (arr) => {
+//     return arr                          // With first sample:
+//         .filter((e) => e % 2 == 0)      // [2, 4, 6, 8, 10]
+//         .map((e) => e / 2)              // [1, 2, 3, 4, 5]
+//         .sort((a, b) => a - b);         // same (since its already ordered)
+// }
+
+//console.log(halfinator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));                      // [1, 2, 3, 4, 5]
+//console.log(evenNumHalfinator([32, 21, 13, 53, 50, 27, 100, 86, 99, 34]));     // (5) [16, 17, 25, 43, 50]
+//console.log(evenNumHalfinator([95, 90, 11, 21, 18, 41, 48, 65, 30, 75]));      // (4) [9, 15, 24, 45]
+
+// passing an array with only odd numbers
+//console.log(evenNumHalfinator([1, 3, 5, 7, 9, 11]));                            // []
+
+// find all roots(of numbers) which are whole numbers
+// const findWholeRootinator = (arr) => {
+//     return arr
+//         .map((e) => Math.sqrt(e))                  // squares each number
+//         .filter((e) => (e - Math.floor(e)) == 0)   // filters out the ones that arent whole/returns the ones that are whole
+// }
+
+//console.log(findWholeRootinator([5, 10, 25]));
+//console.log(findWholeRootinator([2, 3, 6, 12, 14, 16, 23, 26, 30]));
+// #endregion
+
+// More array functions
+// #region .find()
+/* arr.find(callback)
+ - like filter but only returns the first value it finds
+*/
+//console.log(randomNums.find((e => e > 100)));       // 123 3rd element
+// #endregion
+
+// #region .findIndex
+/* arr.findIndex(callBack)
+ - exactly like .find but returns index
+*/
+// console.log(randomNums.findIndex(e => e > 20));             // 1
+// console.log(randomNums.findIndex(e => e < 20));             // 0
+// #endregion
+
+// #region .concat
+/* arr.concat(anotherArr, annotherArr)
+ - merges elements of array onto end of another
+*/
+//  let arr1 = [1, 2, 3];
+//  let arr2 = [4, 5, 6];
+
+// let result = arr1.concat(arr2);
+// console.log(result);                    // [1, 2, 3, 4, 5, 6]
+
+// can do multiple arrays
+// let arr3 = [ 7, 8, 9];
+// let result = arr1.concat(arr2, arr3);   
+// console.log(result);                       // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// can use raw input 
+//let result = arr1.concat(4);
+//console.log(result);
+
+// can mix data types obvi
+// let result = arr1.concat('wowee');
+// console.log(result);
+
+// #endregion
+
+
+
+// Working with arrays of strings
+
